741d2e5f7adc9ca11b3ddc65c55342dba9f28acf
Geoffrey <geoffrey@tourconnect.com>
2015-03-30 22:51:34 -0500

    Integrated input fields into contribute

--- src/scripts/components/contribute.js
+++ src/scripts/components/contribute.js
@@ -2,6 +2,7 @@
 
 var React = require('react/addons');
 var mui = require('material-ui');
+var Procedure = require('./input/input-container');
 
 // CSS
 require('../../styles/contribute.css');
@@ -24,21 +25,21 @@ var ContributePage = React.createClass({
   render: function() {
     var procedures = [];
     for (var i = 0; i < this.state.procedureCount; i++) {
-      procedures.push(<p>Procedure ... </p>);
+      procedures.push(<Procedure />);
     }
     var styles = {
       'padding-left': '20px'
     };
 
     return (
-      <div style={styles}>
+      <div className="contribute" style={styles}>
         <p>Please enter a description of medical services rendered ...</p>
         <mui.TextField
           hintText="Description ..."
           multiLine={true}
           valueLink={this.linkState('description')} />
         {procedures}
-        <mui.RaisedButton label="Add" primary={true} onClick={this._handleAdd} />
+        <mui.RaisedButton className="add-procedure-button" label="Add" primary={true} onClick={this._handleAdd} />
       </div>
     );
   },
